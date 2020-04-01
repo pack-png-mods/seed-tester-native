039a068cdfbfdbfa81a3bef85fbf49e65338c41b
CsalaBalint <balint13>
2020-04-02 00:26:04 +0200

    Added branch for x0-x2

--- generator.h
+++ generator.h
@@ -10,8 +10,6 @@ namespace generator
     class ChunkGenerator
     {
     private:
-        static bool checkWaterfalls(random_math::JavaRand& random);
-        static void simulateDecorations(random_math::JavaRand& random);
         static bool isValidTreeSpot(int treeX, int treeZ, bool firstTreeFound, bool secondTreeFound);
         static bool *generateLeafPattern(random_math::JavaRand& random);
         static int32_t checkTrees(random_math::JavaRand& random, int32_t maxTreeCount);
@@ -20,7 +18,7 @@ namespace generator
         static random_math::LCG advance_774;
         static random_math::LCG advance_830;
         static random_math::LCG advance_3759;
-        static bool populate(int64_t chunkSeed);
+        static bool populate(int64_t chunkSeed, int *uTrees);
 
     };
 }
